1cdc5783f16931866c1a462a68a7d57fabf732af
AlmazKo <a.s.suslov@gmail.com>
2010-10-28 02:39:46 +0400

    добавил дамп для as_Subscribes

--- Brill/Modules/AutoSubmitter/Models/as_Subscribes.php
+++ Brill/Modules/AutoSubmitter/Models/as_Subscribes.php
@@ -20,3 +20,17 @@ class as_Subscribes extends Model {
         'date_end', // дата конца рассылки
     );
 }
+
+/*
+ CREATE TABLE `as_Subscribes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(8) unsigned NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `form` blob NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_begin` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `date_end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
+ */
